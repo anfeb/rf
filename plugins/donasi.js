ee9fef84f2a9729f530fdc445d6a7f163fe1ee16
anfeb <88554978+anfeb@users.noreply.github.com>
2022-01-05 21:20:59 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, fla + 'donasi', `
 ┌「 *Donasi • Emoney* 」
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+├ *Dana*
+├ 087773973701
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi', m)
+`.trim(), '@anfebn', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i

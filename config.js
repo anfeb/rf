2aa6f859f934a8bb01dfbb674ac6cab71b6ef193
anfeb <88554978+anfeb@users.noreply.github.com>
2022-01-11 23:58:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6287773973701', '6285156691677'] // Letakan nomor kamu disini
+global.owner = ['6287773973701',] // Letakan nomor kamu disini
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
